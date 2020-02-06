66608ca7f2b0b0c46449c542f9463d3605ada598
Daniel Nascimento Pimentel <dnprocks@yahoo.com.br>
2020-02-06 18:49:16 -0300

    Routes file

--- src/server.js
+++ src/server.js
@@ -1,14 +1,12 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
-const routes = express.Router();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.disable('x-powered-by');
 
-app.get("/", (req, res) => {
-   return res.send('API de logs beta!')
-})
+app.use(routes);
 
 app.listen(process.env.PORT || 3000);
